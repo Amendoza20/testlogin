90992c9db44cba4090f555f6f57231ec26211c1a
Amendoza20 <20.angel.mendoza@gmail.com>
2020-04-06 14:39:15 -0400

    setting up to deploy

--- src/main/java/com/amm/loginserver/controller/UserController.java
+++ src/main/java/com/amm/loginserver/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.amm.loginserver.controller;
 
+import com.amm.loginserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserService service;
+
+
+
+
 }
